b1d7a0e7d3cd3a9d4b1da7c96f11d9b2c4cd63af
kamalesh-it19 <kamalesh.it19@bitsathy.ac.in>
2019-11-08 21:32:56 +0530

    prog9.c

---
+++ prog9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int i,len;
+    char a[100];
+    gets(a);
+    len=strlen(a);
+    for(i=0;i!=len;i++)
+    {
+        printf("address of character %c = %p \n",a[i],a[i]);
+    }
+    puts(a);
+}
